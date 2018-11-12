ec0800168324f04a3ff2721d87f03d5a55a710f1
Rafael Cruz <ralph1786@gmail.com>
2018-11-12 18:53:00 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,8 +10,8 @@ def argue(phrase)
   return phrase
 end
 
-def greeting(greeting="Hi there", name)
-
+def greeting(greeting="Hi there,", name)
+  return greeting + name +"!"
 end
 
 def return_a_value
